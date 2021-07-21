15f4e13704a9d4c417d005ade19e6570e7af7158
Monica Monteiro <monicamo@gmail.com>
2021-07-21 07:33:43 -0300

    Rota com POST

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,5 +1,9 @@
 module.exports = app => {
   app.get('/atendimentos', (req, res) => {
-    res.send('Vc esta na rota de atendimentos!')
+    res.send('Vc esta na rota de atendimentos, esta fazendo um GET!')
+  })
+
+  app.post('/atendimentos', (req, res) => {
+    res.send('Vc esta na rota de atendimentos, esta fazendo um POST!')
   })
 }
