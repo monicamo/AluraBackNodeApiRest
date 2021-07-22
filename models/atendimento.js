9204ed93dcf0a68be5746e058379952811c1178e
Monica Monteiro <monicamo@gmail.com>
2021-07-22 08:18:23 -0300

    Cadastro com data

--- models/atendimento.js
+++ models/atendimento.js
@@ -1,10 +1,15 @@
+const moment = require('moment')
 const conn = require('../infraestrutura/conexao')
 
 class Atendimento {
   adiciona(atendimento) {
+    const dataCriacao = moment().format('YYYY-MM-DD HH:mm:ss')
+    const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:mm:ss')
+    const atendimentoDatado = { ...atendimento, dataCriacao, data };
+
     const sql = 'INSERT INTO Atendimentos SET ?'
 
-    conn.query(sql, atendimento, (erro, resultados) => {
+    conn.query(sql, atendimentoDatado, (erro, resultados) => {
       if (erro) {
         console.log(erro)
       } else {
