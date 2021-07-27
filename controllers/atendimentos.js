810a3f6003f0cc7564929f1a9df0207f0024b7d1
Monica Monteiro <monicamo@gmail.com>
2021-07-26 22:06:04 -0300

    Mais controllers

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -2,13 +2,16 @@ const Atendimentos = require('../models/atendimento')
 
 module.exports = app => {
   app.get('/atendimentos', (req, res) => {
-    res.send('Vc esta na rota de atendimentos, esta fazendo um GET!')
+    Atendimentos.lista(res);
+  })
+
+  app.get('/atendimentos/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    Atendimentos.buscaPorId(id, res);
   })
 
   app.post('/atendimentos', (req, res) => {
     const atendimento = req.body;
-    
     Atendimentos.adiciona(atendimento, res);
-
   })
 }
--- models/atendimento.js
+++ models/atendimento.js
@@ -2,6 +2,7 @@ const moment = require('moment')
 const conn = require('../infraestrutura/conexao')
 
 class Atendimento {
+
   adiciona(atendimento, res) {
 
     const dataCriacao = moment().format('YYYY-MM-DD HH:mm:ss');
@@ -45,6 +46,31 @@ class Atendimento {
 
     }
 
+  }
+
+  buscaPorId(id, res) {
+    const sql = ` SELECT * from Atendimentos WHERE id=${id}`;
+
+    conn.query(sql, (erro, resultados) => {
+      const atendimento = resultados[0];
+      if (erro) {
+        res.status(400).json(erro);
+      } else {
+        res.status(200).json(atendimento);
+      }
+    })
+  }
+
+  lista(res) {
+    const sql = 'SELECT * FROM Atendimentos';
+
+    conn.query(sql, (erro, resultados) => {
+      if(erro) {
+        res.status(400).json(erro)
+      } else {
+        res.status(200).json(resultados);
+      }
+    })
   }
 }
 
