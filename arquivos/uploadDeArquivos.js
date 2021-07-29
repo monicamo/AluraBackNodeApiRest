11d8eb280349044012195e633d0296fd4c522ae5
Monica Monteiro <monicamo@gmail.com>
2021-07-29 08:08:45 -0300

    Segurança para receber dados externos

--- arquivos/uploadDeArquivos.js
+++ arquivos/uploadDeArquivos.js
@@ -1,9 +1,19 @@
 const fs = require('fs');
+const path = require('path');
 
 module.exports = (caminho, nomeDoArquivo, callbackImagemCriada) => {
 
-  const novoCaminho = `./assets/imagens/${nomeDoArquivo}`;
-  fs.createReadStream(caminho)
-    .pipe(fs.createWriteStream(novoCaminho))
-    .on('finish', () => callbackImagemCriada(novoCaminho) );
+  const tiposValidos =  ['png','jpg','jpeg']
+  const tipo = path.extname(caminho);
+  const tipoEhValido = tiposValidos.indexOf(tipo.substring(1));
+  const novoCaminho = `./assets/imagens/${nomeDoArquivo}${tipo}`;
+
+  if (tipoEhValido === -1) {
+    console.log('ERRO - tipo invalido');
+  } else {
+    fs.createReadStream(caminho)
+      .pipe(fs.createWriteStream(novoCaminho))
+      .on('finish', () => callbackImagemCriada(novoCaminho) );
+  }
+
 }
